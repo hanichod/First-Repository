bf95ad7c941ab4837c4420196710c48e51e04eeb
hanichod <hanichod@yahoo.com>
2022-04-29 14:49:25 -0400

    java project from eclipse

---
+++ project1/src/com/project1/gitDemo.java
@@ -0,0 +1,12 @@
+package com.project1;
+
+public class gitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("Welcome to Java World");
+         System.out.println("craft.common-10");		
+	}
+
+}
